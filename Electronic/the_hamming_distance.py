7bb61556fcfacc93dd75c6822595ff7959d8c105
Paco Brevers <francisco.brevers@tecnilogica.com>
2019-01-21 18:25:15 +0100

    Solved.

---
+++ Electronic/the_hamming_distance.py
@@ -0,0 +1,10 @@
+def checkio(n, m):
+    xor = int(n) ^ int(m)
+    binary_xor = bin(xor)
+    return binary_xor.count('1')
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
