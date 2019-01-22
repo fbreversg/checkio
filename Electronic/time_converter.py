73d14b4b5f2966065f3be7694ca4cda66e2b19cc
Paco Brevers <francisco.brevers@tecnilogica.com>
2019-01-22 16:06:29 +0100

    Solved.

---
+++ Electronic/time_converter.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def time_converter(time):
+
+    return datetime.strptime(time.upper().replace('.', ''), '%I:%M %p').strftime('%H:%M')
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30 p.m.'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30 p.m.') == '12:30'
+    assert time_converter('9:00 a.m.') == '09:00'
+    assert time_converter('11:15 p.m.') == '23:15'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
